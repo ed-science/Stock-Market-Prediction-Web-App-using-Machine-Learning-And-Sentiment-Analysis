c0c3d1b4562511a292c5daf4b7010302178815de
Sourcery AI <>
2022-05-13 11:24:07 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def insertintotable():
         def parser(x):
             return datetime.strptime(x, '%Y-%m-%d')
         def arima_model(train, test):
-            history = [x for x in train]
-            predictions = list()
+            history = list(train)
+            predictions = []
             for t in range(len(test)):
                 model = ARIMA(history, order=(6,1 ,0))
                 model_fit = model.fit(disp=0)
@@ -155,7 +155,7 @@ def insertintotable():
         sc=MinMaxScaler(feature_range=(0,1))#Scaled values btween 0,1
         training_set_scaled=sc.fit_transform(training_set)
         #In scaling, fit_transform for training, transform for test
-        
+
         #Creating data stucture with 7 timesteps and 1 output. 
         #7 timesteps meaning storing trends from 7 days before current day to predict 1 next output
         X_train=[]#memory with 7 days from day i
@@ -172,92 +172,89 @@ def insertintotable():
         X_train=np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))#.shape 0=row,1=col
         X_forecast=np.reshape(X_forecast, (1,X_forecast.shape[0],1))
         #For X_train=np.reshape(no. of rows/samples, timesteps, no. of cols/features)
-        
+
         #Building RNN
         from keras.models import Sequential
         from keras.layers import Dense
         from keras.layers import Dropout
         from keras.layers import LSTM
-        
+
         #Initialise RNN
         regressor=Sequential()
-        
+
         #Add first LSTM layer
         regressor.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
         #units=no. of neurons in layer
         #input_shape=(timesteps,no. of cols/features)
         #return_seq=True for sending recc memory. For last layer, retrun_seq=False since end of the line
         regressor.add(Dropout(0.1))
-        
+
         #Add 2nd LSTM layer
         regressor.add(LSTM(units=50,return_sequences=True))
         regressor.add(Dropout(0.1))
-        
+
         #Add 3rd LSTM layer
         regressor.add(LSTM(units=50,return_sequences=True))
         regressor.add(Dropout(0.1))
-        
+
         #Add 4th LSTM layer
         regressor.add(LSTM(units=50))
         regressor.add(Dropout(0.1))
-        
+
         #Add o/p layer
         regressor.add(Dense(units=1))
-        
+
         #Compile
         regressor.compile(optimizer='adam',loss='mean_squared_error')
-        
+
         #Training
         regressor.fit(X_train,y_train,epochs=25,batch_size=32 )
         #For lstm, batch_size=power of 2
-        
+
         #Testing
         ###dataset_test=pd.read_csv('Google_Stock_Price_Test.csv')
         real_stock_price=dataset_test.iloc[:,4:5].values
-        
+
         #To predict, we need stock prices of 7 days before the test set
         #So combine train and test set to get the entire data set
-        dataset_total=pd.concat((dataset_train['Close'],dataset_test['Close']),axis=0) 
+        dataset_total=pd.concat((dataset_train['Close'],dataset_test['Close']),axis=0)
         testing_set=dataset_total[ len(dataset_total) -len(dataset_test) -7: ].values
         testing_set=testing_set.reshape(-1,1)
         #-1=till last row, (-1,1)=>(80,1). otherwise only (80,0)
-        
+
         #Feature scaling
         testing_set=sc.transform(testing_set)
-        
+
         #Create data structure
-        X_test=[]
-        for i in range(7,len(testing_set)):
-            X_test.append(testing_set[i-7:i,0])
-            #Convert list to numpy arrays
+        X_test = [testing_set[i-7:i,0] for i in range(7,len(testing_set))]
         X_test=np.array(X_test)
-        
+
         #Reshaping: Adding 3rd dimension
         X_test=np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
-        
+
         #Testing Prediction
         predicted_stock_price=regressor.predict(X_test)
-        
+
         #Getting original prices back from scaled values
         predicted_stock_price=sc.inverse_transform(predicted_stock_price)
         fig = plt.figure(figsize=(7.2,4.8),dpi=65)
-        plt.plot(real_stock_price,label='Actual Price')  
+        plt.plot(real_stock_price,label='Actual Price')
         plt.plot(predicted_stock_price,label='Predicted Price')
-          
+
         plt.legend(loc=4)
         plt.savefig('static/LSTM.png')
         plt.close(fig)
-        
-        
+
+
         error_lstm = math.sqrt(mean_squared_error(real_stock_price, predicted_stock_price))
-        
-        
+
+
         #Forecasting Prediction
         forecasted_stock_price=regressor.predict(X_forecast)
-        
+
         #Getting original prices back from scaled values
         forecasted_stock_price=sc.inverse_transform(forecasted_stock_price)
-        
+
         lstm_pred=forecasted_stock_price[0,0]
         print()
         print("##############################################################################")
@@ -268,7 +265,7 @@ def insertintotable():
     #***************** LINEAR REGRESSION SECTION ******************       
     def LIN_REG_ALGO(df):
         #No of days to be forcasted in future
-        forecast_out = int(7)
+        forecast_out = 7
         #Price after n days
         df['Close after n days'] = df['Close'].shift(-forecast_out)
         #New df with only relevant data
@@ -282,25 +279,25 @@ def insertintotable():
         X=np.array(df_new.iloc[:-forecast_out,0:-1])
         #Unknown, X to be forecasted
         X_to_be_forecasted=np.array(df_new.iloc[-forecast_out:,0:-1])
-        
+
         #Traning, testing to plot graphs, check accuracy
         X_train=X[0:int(0.8*len(df)),:]
         X_test=X[int(0.8*len(df)):,:]
         y_train=y[0:int(0.8*len(df)),:]
         y_test=y[int(0.8*len(df)):,:]
-        
+
         # Feature Scaling===Normalization
         from sklearn.preprocessing import StandardScaler
         sc = StandardScaler()
         X_train = sc.fit_transform(X_train)
         X_test = sc.transform(X_test)
-        
+
         X_to_be_forecasted=sc.transform(X_to_be_forecasted)
-        
+
         #Training
         clf = LinearRegression(n_jobs=-1)
         clf.fit(X_train, y_train)
-        
+
         #Testing
         y_test_pred=clf.predict(X_test)
         y_test_pred=y_test_pred*(1.04)
@@ -308,14 +305,14 @@ def insertintotable():
         fig = plt2.figure(figsize=(7.2,4.8),dpi=65)
         plt2.plot(y_test,label='Actual Price' )
         plt2.plot(y_test_pred,label='Predicted Price')
-        
+
         plt2.legend(loc=4)
         plt2.savefig('static/LR.png')
         plt2.close(fig)
-        
+
         error_lr = math.sqrt(mean_squared_error(y_test, y_test_pred))
-        
-        
+
+
         #Forecasting
         forecast_set = clf.predict(X_to_be_forecasted)
         forecast_set=forecast_set*(1.04)
@@ -331,14 +328,14 @@ def insertintotable():
     def retrieving_tweets_polarity(symbol):
         stock_ticker_map = pd.read_csv('Yahoo-Finance-Ticker-Symbols.csv')
         stock_full_form = stock_ticker_map[stock_ticker_map['Ticker']==symbol]
-        symbol = stock_full_form['Name'].to_list()[0][0:12]
+        symbol = stock_full_form['Name'].to_list()[0][:12]
 
         auth = tweepy.OAuthHandler(ct.consumer_key, ct.consumer_secret)
         auth.set_access_token(ct.access_token, ct.access_token_secret)
         user = tweepy.API(auth)
-        
+
         tweets = tweepy.Cursor(user.search, q=symbol, tweet_mode='extended', lang='en',exclude_replies=True).items(ct.num_of_tweets)
-        
+
         tweet_list = [] #List of tweets alongside polarity
         global_polarity = 0 #Polarity of all tweets === Sum of polarities of individual tweets
         tw_list=[] #List of tweets only => to be displayed on web page
@@ -368,20 +365,20 @@ def insertintotable():
             blob = TextBlob(tw)
             polarity = 0 #Polarity of single individual tweet
             for sentence in blob.sentences:
-                   
+
                 polarity += sentence.sentiment.polarity
                 if polarity>0:
                     pos=pos+1
                 if polarity<0:
                     neg=neg+1
-                
+
                 global_polarity += sentence.sentiment.polarity
             if count > 0:
                 tw_list.append(tw2)
-                
+
             tweet_list.append(Tweet(tw, polarity))
-            count=count-1
-        if len(tweet_list) != 0:
+            count -= 1
+        if tweet_list:
             global_polarity = global_polarity / len(tweet_list)
         else:
             global_polarity = global_polarity
@@ -400,23 +397,20 @@ def insertintotable():
         fig1, ax1 = plt.subplots(figsize=(7.2,4.8),dpi=65)
         ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', startangle=90)
         # Equal aspect ratio ensures that pie is drawn as a circle
-        ax1.axis('equal')  
+        ax1.axis('equal')
         plt.tight_layout()
         plt.savefig('static/SA.png')
         plt.close(fig)
+        print()
+        print("##############################################################################")
         #plt.show()
         if global_polarity>0:
-            print()
-            print("##############################################################################")
             print("Tweets Polarity: Overall Positive")
-            print("##############################################################################")
             tw_pol="Overall Positive"
         else:
-            print()
-            print("##############################################################################")
             print("Tweets Polarity: Overall Negative")
-            print("##############################################################################")
             tw_pol="Overall Negative"
+        print("##############################################################################")
         return global_polarity,tw_list,tw_pol,pos,neg,neutral
 
 
@@ -425,21 +419,15 @@ def insertintotable():
             if global_polarity > 0:
                 idea="RISE"
                 decision="BUY"
-                print()
-                print("##############################################################################")
-                print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
-            elif global_polarity <= 0:
+            else:
                 idea="FALL"
                 decision="SELL"
-                print()
-                print("##############################################################################")
-                print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
         else:
             idea="FALL"
             decision="SELL"
-            print()
-            print("##############################################################################")
-            print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
+        print()
+        print("##############################################################################")
+        print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
         return idea, decision
 
 
